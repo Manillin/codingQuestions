28cf173bff7f7641c2fc5af5cbafebf4019eff79
Chris <73969897+Manillin@users.noreply.github.com>
2026-04-12 17:55:24 +0200

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0; i<numCourses;i++){
+            graph.add(new ArrayList<>());
+        }
+
+        int[] indegree = new int[numCourses];
+
+        for(int[]pre: prerequisites){
+            int course= pre[0];
+            int prereq = pre[1];
+            graph.get(prereq).add(course);
+            indegree[course]++;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<numCourses;i++){
+            if(indegree[i] == 0){
+                queue.offer(i);
+            }
+        }
+        int[] order = new int[numCourses];
+        
+        int index = 0;
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            order[index++] = curr;
+
+            for(int next: graph.get(curr)){
+                indegree[next]--;
+                if(indegree[next] == 0){
+                    queue.offer(next);
+                }
+            }
+        }
+        if(index != numCourses){
+            return new int[]{};
+        }
+        return order;
+    }
+}
